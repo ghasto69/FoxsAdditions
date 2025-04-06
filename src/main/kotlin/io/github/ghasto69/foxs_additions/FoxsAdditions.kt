3441c89be0c6c91d9ed99640dc2fbad795602f22
ghasto69 <ghasto69@users.noreply.github.com>
2025-04-06 06:56:56 +0400

    items!

--- src/main/kotlin/io/github/ghasto69/foxs_additions/FoxsAdditions.kt
+++ src/main/kotlin/io/github/ghasto69/foxs_additions/FoxsAdditions.kt
@@ -1,15 +1,20 @@
 package io.github.ghasto69.foxs_additions
 
 import net.fabricmc.api.ModInitializer
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
+@Suppress("unused")
 object FoxsAdditions : ModInitializer {
-    private val logger = LoggerFactory.getLogger("foxs_additions")
+	const val MOD_ID = "foxs_additions"
+    private val LOGGER = LoggerFactory.getLogger(MOD_ID)
+
+	val ITEM = Registry.register(Registries.ITEM, Identifier.of(MOD_ID, "test_item"), Item(Item.Settings()))
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		logger.info("Hello Fabric world!")
+
 	}
 }
